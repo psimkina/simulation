aa0d6ef819f8e53ad5468999805de1ce016a3859
polinasimkina <simkina.polina@gmail.com>
2021-02-12 10:23:28 +0100

    Add file with constant detector parameters.

---
+++ include/ECALConstants.hh
@@ -0,0 +1,10 @@
+#ifndef ECALConstants_h
+#define ECALConstants_h 1
+
+#include "globals.hh"
+
+constexpr G4int kNofCrystals = 5;
+constexpr G4int kNofPlanes = 1;
+constexpr G4int kNofCells = kNofCrystals * kNofCrystals;
+
+#endif
