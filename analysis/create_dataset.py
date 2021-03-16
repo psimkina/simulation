8859e4ca47fd210c4238161844acef13336cf4eb
polinasimkina <simkina.polina@gmail.com>
2021-03-16 09:39:41 +0100

    Add noise to the smearing.

--- analysis/create_dataset.py
+++ analysis/create_dataset.py
@@ -10,7 +10,6 @@ def to_numpy(df, event):
     
     Return:
     - X: numpy array (dim: nx25x25) with simulated energy info.
-    - smeared: numpy array (dim: nx25x25) with smeared energy info.
     - simulated: numpy array (dim: nx7x7) with each simulated cluster in the center of the array.
     - center: numpy array (dim: nx2) with simulated position of the primary particle.
     - energy: numpy array (dim: n) with simulated momentum of primary particle. 
@@ -21,15 +20,14 @@ def to_numpy(df, event):
     """
     # Define empty numpy arrays.
     X = np.full((n, 25, 25), 0.)
-    smeared = np.full((n, 25, 25), 0.)
     simulated = np.full((n, 7, 7), 0.)
     center = np.full((n, 2), 0.)
     energy = np.full((n), 0.)
     
     # Fill the numpy arrays with information from dataframe.  
     for ind in range(n):
+        if (ind%10000 == 0): print(ind)
         X[ind, df.loc[ind].Row, df.loc[ind].Column] = df.loc[ind].EnergyVector
-        smeared[ind, df.loc[ind].Row, df.loc[ind].Column] = df.loc[ind].EnergySmeared
         center[ind, 0], center[ind, 1] = event.loc[ind].InitialRow, event.loc[ind].InitialColumn
         energy[ind] = event.loc[ind].InitialMomentum
         
@@ -40,26 +38,26 @@ def to_numpy(df, event):
                                                                 event.loc[ind].InitialColumn+4)))]
         k = one_event.Row - int(event.loc[ind].InitialRow) + 3
         l = one_event.Column - int(event.loc[ind].InitialColumn) + 3
-        simulated[ind, k, l] = one_event.EnergySmeared
+        simulated[ind, k, l] = one_event.EnergyVector
         
-    return X, smeared, simulated, center, energy
+    return X, simulated, center, energy
 
-X, sme, sim, center, en = to_numpy(df, event)
+X, sim, center, en = to_numpy(df, event)
 
-def create_sample(X, sme, sim, center, en, k=3):
+n = 50000
+
+def create_sample(X, sim, center, en, k=1):
     """
     Randomly combines clusters from different events into one sample and returns data to be fed 
     into the network:
     
     - X_sim: numpy array (dim: 25x25) containing simulated energy information of the clusters.
-    - X_sme: numpy array (dim: 25x25) containing smeared energy information of the clusters.
+    - ysim: numpy array (dim: kx7x7) with sim energy per pixel info of each cluster.
     - ysort: numpy array (dim: kx2) with center sim coordinates of each cluster.
     - ye: numpy array (dim: k) with sim energy info of each cluster.
-    - ysim: numpy array (dim: kx7x7) with sim energy per pixel info of each cluster.
     
     Args: 
     - X: numpy array with simulated energy info. 
-    - sme: numpy array with smeared energy info.
     - sim: numpy array with energy deposition per pixel. 
     - center: numoy array with sim coordinates of the cluster center. 
     - en: numpy array with total energy info for each cluster.
@@ -67,10 +65,9 @@ def create_sample(X, sme, sim, center, en, k=3):
     """
     # Randomly choose events and the number of clusters and fill the arrays. 
     random.random()
-    index = random.sample(list(np.arange(0,n)), k)
+    index = random.sample(list(np.arange(0, n)), k)
 
     X_sim = sum((X[ind] for ind in index))
-    X_sme = sum((sme[ind] for ind in index))
     yc = np.asarray([(center[ind]) for ind in index])
     ye = np.asarray([(en[ind]) for ind in index])
     ysim = np.asarray([(sim[ind]) for ind in index])
@@ -81,10 +78,10 @@ def create_sample(X, sme, sim, center, en, k=3):
     ye = ye[np.argsort((np.sqrt(np.power(yc[:,0],2) + np.power(yc[:,1],2))//dr + yc[:,0]/25))] 
     ysim = ysim[np.argsort((np.sqrt(np.power(yc[:,0],2) + np.power(yc[:,1],2))//dr + yc[:,0]/25))] 
 
-    return X_sim, X_sme, ysort, ye, ysim
-X, X_sme, yc, ye, ysim = create_sample(X, sme, sim, center, en) 
+    return X_sim, ysim, ysort, ye
+#X, X_sme, yc, ye, ysim = create_sample(X, sme, sim, center, en) 
 
-def save_sample(name, nevt=1000, s=0):
+def save_sample(name, nevt=10, s=0, k=1):
     """
     Create and save data sample in .npy format.
     
@@ -92,25 +89,43 @@ def save_sample(name, nevt=1000, s=0):
     - name: name of the file to save into.
     - nevent (default = 10000): number of samples to save in one file.
     - s (default = 0): random seed.
+    - k (default = 1): number of clusters per image. 
     """
     random.seed(s)
     # Create numpy arrays to be filled. 
-    X = np.full((nevt, 25, 25), 0.)
-    yc = np.full((nevt, 3, 2), 0.)
-    ysim = np.full((nevt, 3, 7, 7), 0.)
-    #ye = np.full((nevt, 5), 0.)
+    Xsim = np.full((nevt, 25, 25), 0.)
+    yc = np.full((nevt, k, 2), 0.)
+    ysim = np.full((nevt, k, 7, 7), 0.)
+    ye = np.full((nevt, k), 0.)
     
     # Fill the arrays.
     for i in range(nevt):
-        simul, X[i], yc[i], ener, ysim[i] = create_sample(X, sme, sim, center, en)
+        if (i%10000 == 0): print('samples made: ', i)
+        Xsim[i], ysim[i], yc[i], ye[i] = create_sample(X, sim, center, en, k)
+
+    # Smearing of the simultation. 
+    Xsim = np.random.normal(Xsim, 0.1) # sigma = 100 MeV.
+    Xsim[Xsim<0.05] = 0.0 # cut on smeared energy 50 MeV. 
+    
+    print('Start smearing')
+    
+    ysme = np.full((nevt, k, 7, 7), 0.)
+    for i, sample in enumerate(Xsim):
+        for j in range(k):
+            array = sample[max(int(yc[i,j,0])-3,0):min(25,int(yc[i,j,0])+4),max(0,int(yc[i,j,1])-3):min(int(yc[i,j,1])+4,25)]
+            ysme[i,j] = np.pad(array, ((0,7-array.shape[0]),(0,7-array.shape[1])))
+            
     # Normalize the center coordinates to (0,1). 
     yc/=25
-    X = X.reshape(nevt,25,25,1)
+    # Reshape to fir the input for the neural network.
+    Xsim = Xsim.reshape(nevt,25,25,1)
         
     # Save files. 
-    np.save('X' + str(name), X)
+    np.save('X' + str(name), Xsim)
     np.save('yc' + str(name), yc)
     np.save('ysim' + str(name), ysim)
+    np.save('en' + str(name), ye)
+    np.save('ysme' + str(name), ysme)
     
 
     
--- analysis/ntuple_loader.py
+++ analysis/ntuple_loader.py
@@ -13,31 +13,5 @@ event = event.reset_index(level=1, drop=True)
 # Second dataframe with energy deposits of each event. 
 df['Row'] = df['Row'].astype(int)
 df['Column'] = df['Column'].astype(int)
-df['EnergyVector']/=1000
-df = df[['Row','Column', 'EnergyVector']][df['EnergyVector']>0.05] # Condition on what values to leave in the df. 
-
-# Define the smeared energy at each cell.
-def get_parameters(E, a=0.02, b=0., c=0.):
-    """
-    Function to calculate the parameters (based on energy resolution) 
-    to be fed into the gaussian distribution. 
-    
-    Return: 
-    - mu: mean of the distribution (energy in this case). 
-    - sigma: standard deviation. 
-    
-    Args: 
-    - E: true energy (in MeV) deposited in the cell.
-    - a (default=0.03): stochastic term of energy resolution. 
-    - b (default=250 MeV): noise term of energy resolution. 
-    - c (default=0.005): constant term of energy resolution. 
-    """
-    mu = E
-    sigma = E*np.sqrt((a/np.sqrt(E))**2 + (b/E)**2 + c**2)
-    return mu, sigma 
-
-# Calculate the smeared energy and add it to the df. 
-mu, sigma = get_parameters(df['EnergyVector'])
-smear = np.random.normal(mu, sigma) 
-df['EnergySmeared'] = smear
-df['EnergySmeared'][df['EnergySmeared']<0.05] = 0
+df['EnergyVector']/=1000.
+df = df[['Row','Column', 'EnergyVector']][df['EnergyVector']>0.0] # Condition on what values to leave in the df. 
