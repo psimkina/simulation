a0ab4a7c6ad546c87ed1572b63cc098a92317c73
Polina Simkina <polina.simkina@cern.ch>
2021-11-14 11:32:04 +0100

    Update for the new files.

--- analysis/create_dataset.py
+++ analysis/create_dataset.py
@@ -1,8 +1,12 @@
-from ntuple_loader import df, event
+#from ntuple_loader import df, event
 import numpy as np 
 import random
+import pandas as pd
 
-n = event.index.size
+#df = pd.read_pickle('df')
+#event = pd.read_pickle('ev')
+
+n = 10000
 print("Number of events: ", n)
 def to_numpy(df, event):
     """
@@ -19,34 +23,39 @@ def to_numpy(df, event):
     - event: dataframe containing information about primary event. 
     """
     # Define empty numpy arrays.
-    X = np.full((n, 25, 25), 0.)
+    X = np.full((n, 51, 51), 0.)
     simulated = np.full((n, 7, 7), 0.)
     center = np.full((n, 2), 0.)
     energy = np.full((n), 0.)
+    ptype = np.full((n), 0.)
     
     # Fill the numpy arrays with information from dataframe.  
     for ind in range(n):
-        if (ind%10000 == 0): print(ind)
-        X[ind, df.loc[ind].Row, df.loc[ind].Column] = df.loc[ind].EnergyVector
+        if (ind%10000 == 0): print("samples processed:", ind)
+        row = df.loc[ind].Row.astype(int)
+        col = df.loc[ind].Column.astype(int)
+        X[ind, row, col] = df.loc[ind].EnergyVector
         center[ind, 0], center[ind, 1] = event.loc[ind].InitialRow, event.loc[ind].InitialColumn
         energy[ind] = event.loc[ind].InitialMomentum
+        ptype[ind] = event.loc[ind].ParticleType
+        
+        row_init = round(event.loc[ind].InitialRow).astype(int)
+        col_init = round(event.loc[ind].InitialColumn).astype(int)
         
         # Change the position of the cluster, so that it is in the center of the numpy array. 
         one_event = df.loc[ind]
-        one_event = one_event[(one_event.Row.isin(np.arange(event.loc[ind].InitialRow-3,event.loc[ind].InitialRow+4)))\
-                              &(one_event.Column.isin(np.arange(event.loc[ind].InitialColumn-3,\
-                                                                event.loc[ind].InitialColumn+4)))]
-        k = one_event.Row - int(event.loc[ind].InitialRow) + 3
-        l = one_event.Column - int(event.loc[ind].InitialColumn) + 3
-        simulated[ind, k, l] = one_event.EnergyVector
+        one_event = one_event[(one_event.Row.isin(np.arange(row_init-3,row_init+4)))\
+                              &(one_event.Column.isin(np.arange(col_init-3,col_init+4)))]
         
-    return X, simulated, center, energy
+        k = one_event.Row.astype(int) - row_init + 3
+        l = one_event.Column.astype(int) - col_init + 3
+        simulated[ind, k, l] = one_event.EnergyVector
 
-X, sim, center, en = to_numpy(df, event)
+    return X, simulated, center, energy, ptype
 
-n = 50000
+#X, sim, center, en, partype = to_numpy(df, event)
 
-def create_sample(X, sim, center, en, k=1):
+def create_sample(X, sim, center, en, partype, k=1):
     """
     Randomly combines clusters from different events into one sample and returns data to be fed 
     into the network:
@@ -65,23 +74,29 @@ def create_sample(X, sim, center, en, k=1):
     """
     # Randomly choose events and the number of clusters and fill the arrays. 
     random.random()
-    index = random.sample(list(np.arange(0, n)), k)
+    t = random.randint(1, k)
+    index = random.sample(list(np.arange(0, n)), t)
 
     X_sim = sum((X[ind] for ind in index))
     yc = np.asarray([(center[ind]) for ind in index])
     ye = np.asarray([(en[ind]) for ind in index])
+    yp = np.asarray([(partype[ind]) for ind in index])
     ysim = np.asarray([(sim[ind]) for ind in index])
     
     # Sort the clusters based on the distance from (0,0) coordinate of the image. 
     dr = np.sqrt(1**2 + 1**2)
-    ysort = yc[np.argsort((np.sqrt(np.power(yc[:,0],2) + np.power(yc[:,1],2))//dr + yc[:,0]/25))] 
-    ye = ye[np.argsort((np.sqrt(np.power(yc[:,0],2) + np.power(yc[:,1],2))//dr + yc[:,0]/25))] 
-    ysim = ysim[np.argsort((np.sqrt(np.power(yc[:,0],2) + np.power(yc[:,1],2))//dr + yc[:,0]/25))] 
+    ye = ye[np.argsort(-yc[:,0])] 
+    yp = yp[np.argsort(-yc[:,0])] 
+    ysim = ysim[np.argsort(-yc[:,0])]
+    ysort = yc[np.argsort(-yc[:,0])] 
 
-    return X_sim, ysim, ysort, ye
+    return X_sim, np.pad(ysim,((0, k-t),(0,0), (0,0)), constant_values=0.),\
+        np.pad(ysort, ((0,k-t),(0,0)), constant_values=-1),\
+        np.pad(ye, (0,k-t), constant_values=-1),\
+        np.pad(yp, (0,k-t), constant_values=-1)
 #X, X_sme, yc, ye, ysim = create_sample(X, sme, sim, center, en) 
 
-def save_sample(name, nevt=10, s=0, k=1):
+def save_sample(name, X, sim, center, en, partype, nevt=10, s=0, k=1):
     """
     Create and save data sample in .npy format.
     
@@ -93,15 +108,16 @@ def save_sample(name, nevt=10, s=0, k=1):
     """
     random.seed(s)
     # Create numpy arrays to be filled. 
-    Xsim = np.full((nevt, 25, 25), 0.)
+    Xsim = np.full((nevt, 51, 51), 0.)
     yc = np.full((nevt, k, 2), 0.)
     ysim = np.full((nevt, k, 7, 7), 0.)
     ye = np.full((nevt, k), 0.)
+    yp = np.full((nevt, k), 0.)
     
     # Fill the arrays.
     for i in range(nevt):
         if (i%10000 == 0): print('samples made: ', i)
-        Xsim[i], ysim[i], yc[i], ye[i] = create_sample(X, sim, center, en, k)
+        Xsim[i], ysim[i], yc[i], ye[i], yp[i] = create_sample(X, sim, center, en, partype, k)
 
     # Smearing of the simultation. 
     Xsim = np.random.normal(Xsim, 0.1) # sigma = 100 MeV.
@@ -112,20 +128,23 @@ def save_sample(name, nevt=10, s=0, k=1):
     ysme = np.full((nevt, k, 7, 7), 0.)
     for i, sample in enumerate(Xsim):
         for j in range(k):
-            array = sample[max(int(yc[i,j,0])-3,0):min(25,int(yc[i,j,0])+4),max(0,int(yc[i,j,1])-3):min(int(yc[i,j,1])+4,25)]
+            array = sample[max(int(yc[i,j,0])-3,0):min(51,int(yc[i,j,0])+4),max(0,int(yc[i,j,1])-3):min(int(yc[i,j,1])+4,51)]
             ysme[i,j] = np.pad(array, ((0,7-array.shape[0]),(0,7-array.shape[1])))
-            
+    
+    ysim[ysim<0.05] = 0.0
+    ysme[ysme<0.05] = 0.0
     # Normalize the center coordinates to (0,1). 
-    yc/=25
-    # Reshape to fir the input for the neural network.
-    Xsim = Xsim.reshape(nevt,25,25,1)
+    yc/=51.
+    # Reshape to fit the input for the neural network.
+    Xsim = Xsim.reshape(nevt,51,51,1)
         
     # Save files. 
-    np.save('X' + str(name), Xsim)
-    np.save('yc' + str(name), yc)
-    np.save('ysim' + str(name), ysim)
-    np.save('en' + str(name), ye)
-    np.save('ysme' + str(name), ysme)
+    np.save(str(name) + 'X', Xsim)
+    np.save(str(name)+'yc', yc)
+    np.save(str(name)+'ysim', ysim)
+    np.save(str(name)+'en', ye)
+    np.save(str(name)+'ysme', ysme)
+   # np.save(str(name)+'yptype', yp)
     
 
     
--- analysis/ntuple_loader.py
+++ analysis/ntuple_loader.py
@@ -1,17 +1,33 @@
 import uproot
 import numpy as np
+import pandas as pd
 
-# Load ntuple from the files and convert it to pandas dataframe.
-upFile = uproot.open('~/calo_sim/simulation/build/dataCaloHits.root')
-tree = upFile['CaloH']
-df = tree.pandas.df()
+def create_df(filename="splitted_0-24999_events.root"):
+    # Load ntuple from the files and convert it to pandas dataframe.
+    #upFilePi = uproot.open('~/calo_sim/simulation/build/pions.root')
+    with uproot.open("~/calo_sim/simulation/build/data/"+str(filename)) as file:
+        #treePi = upFilePi['CaloH']
+        treePh = file['CaloH']
+        #dfPi = treePi.pandas.df()
+        df = treePh.pandas.df()
 
-# First dataframe with information about primary particle. 
-event = df[['TotalEn', 'InitialMomentum','InitialRow', 'InitialColumn']].drop_duplicates()
-event = event.reset_index(level=1, drop=True)
+    df['ParticleType'] = 0
+    #dfPi['ParticleType'] = 1
+    #print('done')
+    #df = df.loc[50000:59999]
+    print('done - 2')
+    #dfPi = dfPi.loc[25000:]
 
-# Second dataframe with energy deposits of each event. 
-df['Row'] = df['Row'].astype(int)
-df['Column'] = df['Column'].astype(int)
-df['EnergyVector']/=1000.
-df = df[['Row','Column', 'EnergyVector']][df['EnergyVector']>0.0] # Condition on what values to leave in the df. 
+    #df = pd.concat([dfPh, dfPi])
+
+    # First dataframe with information about primary particle. 
+    event = df[['TotalEn', 'InitialMomentum','InitialRow', 'InitialColumn', 'ParticleType']].drop_duplicates()
+    event = event.reset_index(level=1, drop=True)
+    print('done - 3')
+    # Second dataframe with energy deposits of each event. 
+    df = df[['Row','Column', 'EnergyVector']][df['EnergyVector']>0.0] # Condition on what values to leave in the df. 
+    print('done - 4')
+    #df['Row'] = df['Row'].astype(int)
+    #df['Column'] = df['Column'].astype(int)
+    df['EnergyVector']/=1000.
+    return df, event
